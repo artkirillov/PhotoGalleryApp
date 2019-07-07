cbe21e48ba774fb1cdcc6635e4a0dd2678afb76b
Artem Kirillov <artemkirillov@yandex-team.ru>
2019-07-07 13:29:16 +0300

    Initial Commit

---
+++ Look At This/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Look At This
+//
+//  Created by Artem Kirillov on 07/07/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
